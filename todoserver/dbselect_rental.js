83a623ab1e7fac2081f1f5aac447f48dc615c598
ArcTorralba <atorralba_190000002742@uic.edu.ph>
2021-10-07 21:17:33 +0800

    init

---
+++ todoserver/dbselect_rental.js
@@ -0,0 +1,13 @@
+const pool = require("./db");
+const sql = "SELECT * FROM rental.todo";
+
+function result(err, res){
+    try{
+        console.log(res.rows);        
+    }catch(err){
+        console.error(err.message);
+    }
+}
+
+pool.query(sql,result);
+pool.end();
